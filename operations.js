c00fc50badce60c6b5074de3a75dae71dc809384
Eeister-Git <55165323+Eeister-Git@users.noreply.github.com>
2021-06-11 00:49:11 -0400

    Add files via upload
    can do stringed operations. if you string it like 8+9 and add some operation button then after 8+9 you can hit any operation button to confirm the results and hit again to chose next operation you want.

--- operations.js
+++ operations.js
@@ -20,24 +20,39 @@ function div(a, b){
 }
 
 function operate(a, b, c){
+    a = parseFloat(a);
+    b = parseFloat(b);
     if( c === "+"){
-        console.log(add(a,b));
+        return (add(a,b));
     }
     else if ( c === "-"){
-        console.log(sub(a,b));
+        return (sub(a,b));
     }
     else if ( c === "*"){
-        console.log(mul(a,b));
+        return (mul(a,b));
     }
     else if ( c === "/"){
-        console.log(sub(a,b));
+        return (div(a,b));
     }
 }
 
 function display(){
     let screen = document.getElementsByClassName("screen")[0];
-    let history = [];
+    let history = "";
+    let operator = "";
+    let operatortwo = "";
     screen.innerHTML = "";
+    
+    // window.addEventListener("keypress", function(e){
+    //     console.log(e.key);
+
+    //     if (Number.isInteger(parseInt(e.key)) == true){
+    //         if(screen.innerHTML.length == 18){
+    //             return
+    //         }
+    //         screen.innerHTML += e.key.toString();
+    //     }
+    // }); 
 
     $("button").click(function() {
         var fired_button = $(this).val();
@@ -50,19 +65,39 @@ function display(){
         }
         else{
             if (fired_button == "clear"){
-                screen.innerHTML = ""
-            }
+                screen.innerHTML = "";
+                history = "";
+                operator = "";
+                operatortwo = "";
+             }
             else if(fired_button == "backspace"){
                 screen.innerHTML = screen.innerHTML.slice(0,-1);
             }
             else if(fired_button == "."){
                 if(screen.innerHTML.includes(".")){
-                    return
+                    return;
                 }
-                screen.innerHTML += "."
+                screen.innerHTML += ".";
+            }
+            else if(fired_button == "="){
+                if(history != ""){
+                    screen.innerHTML = operate(history, screen.innerHTML, operator);
+                    operator = "";
+                }
             }
             else{
-                history.push(screen.innerHTML);
+                if(operator == ""){
+                    operator = fired_button;
+                    history = screen.innerHTML;
+                    screen.innerHTML = "";
+                    console.log("hey" + operator)
+                }
+                else{
+                    console.log(history, screen.innerHTML);
+                    screen.innerHTML = operate(history, screen.innerHTML, operator);
+                    operator = "";
+                    console.log("hi" + operator)
+                }
             }
         }
     });
